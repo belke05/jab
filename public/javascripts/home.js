31a658fbb2b00ae5caf5c4a2c7787d69a98e1d50
belke05 <you@example.com>
2019-09-09 23:02:50 +0200

    client script edits

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,7 +1,19 @@
+import Axios from "axios";
+
 const images = document.querySelectorAll("img");
+const article_like_btn = document.querySelectorAll("article interact like_btn");
 
 for (let i = 0; i < images.length; i++) {
   if (images[i].currentSrc == "") {
     images[i].hidden = true;
   }
 }
+
+function addLikes(evt) {
+  evt_target;
+  Axios.post()
+    .then()
+    .catch();
+}
+
+article_like_btn.onclick = addLikes;
