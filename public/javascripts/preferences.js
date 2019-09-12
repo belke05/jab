e50c089a3981b6a489e622029bc362273052ebc8
belke05 <you@example.com>
2019-09-12 18:26:42 +0200

    jab logo hover on pref page

--- public/javascripts/preferences.js
+++ public/javascripts/preferences.js
@@ -38,65 +38,82 @@ function toggleFighterStyle(evt) {
 }
 
 Fighters.forEach(fighter => {
-  fighter.onclick = (evt) =>{
-    toggleFighterStyle(evt)
-    btnEditFighter.innerText = "Save Changes"
+  fighter.onclick = evt => {
+    toggleFighterStyle(evt);
+    btnEditFighter.innerText = "Save Changes";
     btnEditFighter.classList.remove("btn-after-edit");
     btnEditFighter.classList.add("btn-edit");
-  }
+  };
 });
 
-
-btnEditFighter.onclick = (evt) =>{
+btnEditFighter.onclick = evt => {
   let selectedFighterContainer = document.querySelector(".chosen-fighter");
   console.log(selectedFighterContainer);
   let selectedFighter = selectedFighterContainer.querySelector(".pref-logo");
-  postNewFighter(selectedFighter,evt);
-  btnEditFighter.innerText = "Changes Saved"
+  postNewFighter(selectedFighter, evt);
+  btnEditFighter.innerText = "Changes Saved";
   btnEditFighter.classList.add("btn-after-edit");
   btnEditFighter.classList.remove("btn-edit");
-}
+};
 
 function postNewFighter(selectedFighter, evt) {
   evt.preventDefault();
   axios
-    .post("/newFighter", {fighter_id : selectedFighter.id })
+    .post("/newFighter", { fighter_id: selectedFighter.id })
     .then(newFighter => {
-      console.log(newFighter)
+      console.log(newFighter);
     })
     .catch(serverErr => console.log(serverErr));
 }
 
 Sports.forEach(sport => {
-  sport.onclick = ()=>{
+  sport.onclick = () => {
     sport.classList.toggle("unchoosen-logo");
-    sport.classList.toggle("choosen-logo")
+    sport.classList.toggle("choosen-logo");
     sport.parentElement.classList.toggle("unselected-container");
-    btnEditSports.innerText = "Save Changes"
+    btnEditSports.innerText = "Save Changes";
     btnEditSports.classList.remove("btn-after-edit");
     btnEditSports.classList.add("btn-edit");
-  }
+  };
 });
 
-function updateSports(selectedSports, evt){
+function updateSports(selectedSports, evt) {
   evt.preventDefault();
   axios
-  .post("/newSports", {selectedSports : selectedSports})
-  .then (newSports =>{
-    console.log(newSports)
-  })
-  .catch(serverErr=> console.log(serverErr))
-  btnEditSports.innerText = "Changes Saved"
+    .post("/newSports", { selectedSports: selectedSports })
+    .then(newSports => {
+      console.log(newSports);
+    })
+    .catch(serverErr => console.log(serverErr));
+  btnEditSports.innerText = "Changes Saved";
   btnEditSports.classList.add("btn-after-edit");
   btnEditSports.classList.remove("btn-edit");
 }
 
-btnEditSports.onclick = (evt) =>{
+btnEditSports.onclick = evt => {
   let selectedSports = document.querySelectorAll(".choosen-logo");
   let sportsArray = [];
-  selectedSports.forEach(sport =>{
-    sportsArray.push(sport.id)
-  })
+  selectedSports.forEach(sport => {
+    sportsArray.push(sport.id);
+  });
   updateSports(sportsArray, evt);
+};
+
+var logo = document.getElementById("main_logo");
+
+logo.onmouseenter = hoverLogo;
+logo.onmouseleave = basicLogo;
+
+function hoverLogo() {
+  logo.src = "/images/logo/hover.png";
+}
+function basicLogo() {
+  logo.src = "/images/logo/basic.png";
 }
 
+var burger = document.getElementById("burger");
+var sidebar = document.getElementById("sidebar");
+
+burger.onclick = () => {
+  sidebar.classList.toggle("is-here"), console.log("hello");
+};
