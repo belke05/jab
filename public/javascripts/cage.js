3dfddc56adf6532112444299c2327e2210a6d367
belke05 <you@example.com>
2019-09-12 10:40:55 +0200

    addition of personal fighter articles

---
+++ public/javascripts/cage.js
@@ -0,0 +1,197 @@
+const article_like_btn = document.querySelectorAll(
+  ".article .interact .like_btn"
+);
+const tags_box = document.querySelectorAll(".tags_li input");
+const articles_container = document.querySelector(".articles");
+const comment_btns = document.querySelectorAll(".comment_btn");
+const articles = document.querySelectorAll(".article");
+const inputbox = document.querySelectorAll("input");
+const inputicon = document.querySelectorAll(".addCommentIcon");
+const userFighter = document.querySelector(".user_gif_div img").alt;
+const url = `https://newsapi.org/v2/everything?q=${userFighter}&apiKey=537b32f4c8894d2b8cf98f3b990d3e3f`;
+console.log(url);
+
+tags_box.forEach(box => {
+  box.onclick = findCategory;
+});
+inputbox.forEach(input => {
+  input.onkeydown = addComment;
+});
+inputicon.forEach(icon => {
+  icon.onclick = addComment;
+});
+
+ApiCallFighterArticles();
+function ApiCallFighterArticles() {
+  axios
+    .get(url)
+    .then(res => {
+      console.log(res);
+      const articles = res.data.articles.slice(0, 2);
+      appendArticles(articles);
+    })
+    .post(err => {
+      console.log(err);
+    });
+}
+
+function appendArticles(articles) {
+  let container_figther_articles = document.createElement("div");
+  container_figther_articles.className = "fighter_articles";
+  container_figther_articles.innerHTML = `<h2 class="article_section_title">Your latest ${userFighter} updates:</h2>`;
+  articles.forEach(art => {
+    let art_container = document.createElement("div");
+    art_container.className = "fighter_article article";
+    art_container.innerHTML = `
+      <a href="${art.url}"><img class="articleImage" src="${art.urlToImage}" alt="${art.title}"></a>
+      <div class="articleDiv">
+          <a href="${art.url}">
+              <h2 class="articleTitle">${art.title}</h2>
+          </a>
+          <a href="${art.url}">
+              <p class="articleDesc">${art.description}</p>
+          </a>
+      </div>
+    `;
+    container_figther_articles.appendChild(art_container);
+  });
+  articles_container.insertBefore(
+    container_figther_articles,
+    articles_container.firstChild
+  );
+
+  const images = document.querySelectorAll(".fighter_articles img");
+  console.log(images);
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].currentSrc == "") {
+      images[i].hidden = true;
+    } else {
+      images[i].hidden = false;
+    }
+  }
+}
+
+function addLikes(evt) {
+  evt.preventDefault();
+  let { btn, art } = getArticleId(evt);
+  axios
+    .post("/addlike", { id: art.id })
+    .then(dbRes => {
+      if (dbRes.data === "already jabbed") {
+        console.log(dbRes);
+        console.log("al jabbed");
+        removeJab(btn);
+      } else {
+        addJab(btn);
+        console.log("new jab");
+        console.log(dbRes);
+      }
+    })
+    .catch(dbErr => {
+      console.log(dbErr);
+    });
+}
+
+function addJab(butn) {
+  console.log(butn.innerText.replace("jabs", ""));
+  console.log(butn.innerText.trim(), "-----");
+  const newNum = Number(butn.innerText.trim());
+  butn.innerHTML = `<i class="fas fa-fist-raised"></i>  ${newNum + 1}`;
+}
+
+function removeJab(butn) {
+  console.log(butn.innerText.trim(), "-----");
+  const newNum = Number(butn.innerText.trim());
+  if (newNum != 0) {
+    butn.innerHTML = `<i class="far fa-hand-paper"></i>  ${newNum - 1}`;
+  }
+}
+
+function commentinput(evt) {
+  let { btn, art } = getArticleId(evt);
+  displaycommentbox(art);
+  addtextInput(art);
+  console.log(btn);
+  console.log(art.id);
+}
+
+function addtextInput(art) {
+  const inputbox = document
+    .getElementById(`${art.id}`)
+    .parentElement.querySelectorAll("input");
+  const inputicon = document.querySelectorAll(".addCommentIcon");
+  inputbox.forEach(input => {
+    input.onkeydown = addComment;
+  });
+  inputicon.forEach(icon => {
+    icon.onclick = addComment;
+  });
+}
+
+function getArticleId(evt) {
+  evt.preventDefault();
+  let btn;
+  let art;
+  if (evt.target.tagName === "I") {
+    btn = evt.target.parentElement;
+    art = btn.parentElement;
+  } else {
+    btn = evt.target;
+    art = btn.parentElement;
+  }
+  return { btn, art };
+}
+
+function addComment(evt) {
+  console.log(evt);
+  if (evt.keyCode == 13 || evt.type == "click") {
+    const art = evt.target.parentElement.parentElement.parentElement.querySelector(
+      ".interact"
+    );
+    const art_id = art.id;
+    const comment = art.parentElement.querySelector("input").value;
+    const listItem = document.createElement("li");
+    axios
+      .post("/addComment", { comment: comment, art_id: art_id })
+      .then(dbRes => {
+        listItem.innerText = `${dbRes.data} ` + comment;
+        art.parentElement
+          .querySelector(".comment_display")
+          .appendChild(listItem);
+      })
+      .catch(dbErr => {
+        console.log(dbErr);
+      });
+  }
+}
+
+function displaycommentbox(art) {
+  console.log("hererere", art);
+  const comment_section = art.parentElement.querySelector(".comment_display");
+  console.log(comment_section, "ggggggg");
+  comment_section.classList.toggle("hide_comments");
+}
+
+article_like_btn.forEach(btn => {
+  btn.onclick = addLikes;
+});
+
+comment_btns.forEach(btn => {
+  btn.onclick = commentinput;
+});
+
+window.onscroll = function() {
+  myFunction();
+};
+
+var main = document.getElementById("main");
+var header = document.getElementById("taglist");
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    // console.log(window.pageYOffset)
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,101 +1,102 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const protectedRoute = require("../middleware/protectedRoute")
+const protectedRoute = require("../middleware/protectedRoute");
 const uploaderMiddleware = require("../config/cloudinary.js");
 const Users = require("./../models/users.js");
 const Fighters = require("../models/fighters.js");
 const Leagues = require("./../models/leagues.js");
 const Article = require("./../models/articles.js");
 
-
-router.get("/preferences",protectedRoute,(req,res)=>{
-  let userId= req.session.currentUser._id;
+router.get("/preferences", protectedRoute, (req, res) => {
+  let userId = req.session.currentUser._id;
   var user;
   Users.findById(userId)
-    .then(userRes=>{
-      console.log("user found", userRes)
+    .then(userRes => {
+      console.log("user found", userRes);
       user = userRes;
       userLeaguesIds = userRes.leagues;
-      Fighters.find()
-      .then(fighterListRes => {
+      Fighters.find().then(fighterListRes => {
         console.log("all fighters found", fighterListRes);
-        Fighters.findById(userRes.fighter)
-        .then(fighterRes=>{
-          console.log("Favorite Fighter Found", fighterRes)
+        Fighters.findById(userRes.fighter).then(fighterRes => {
+          console.log("Favorite Fighter Found", fighterRes);
           user.fighterName = fighterRes.name;
           user.fighterImgPath = fighterRes.imgPath;
-          var date = new Date(user.created)
+          var date = new Date(user.created);
           user.creationDate = date.toDateString();
-          Leagues.find()
-          .then (leaguesList => {
-            console.log("all leagues found")
+          Leagues.find().then(leaguesList => {
+            console.log("all leagues found");
             res.render("userPref/preferences", {
-              title: "cage", 
+              title: "cage",
               user: user,
               leagues: leaguesList,
               fighters: fighterListRes,
-              scripts: ["../javascripts/preferences.js"],
+              scripts: ["../javascripts/preferences.js"]
             });
-          })
-        })
-      })
-
+          });
+        });
+      });
     })
-    .catch(userErr=> console.log(userErr))
+    .catch(userErr => console.log(userErr));
 });
 
 router.get("/cage", protectedRoute, (req, res) => {
   const userId = req.session.currentUser._id;
-    console.log(req.session ,"ici !");
-    Users.findById(userId)
-    .then(user=>{
-      Article.find({league:{$in : user.leagueTag}})
-      .then(article=>{
-        console.log("************************")
-        console.log(article);
-        Fighters.findById(user.fighter)
-        .then(fighter=>{
-          console.log(fighter)
-          res.render("userPref/cage",{
-            article:article,
-            userPref : {
-              fighter : fighter,
+  console.log(req.session, "ici !");
+  Users.findById(userId).then(user => {
+    Article.find({ league: { $in: user.leagueTag } }).then(article => {
+      console.log("************************");
+      console.log(article);
+      Fighters.findById(user.fighter)
+        .then(fighter => {
+          console.log(fighter);
+          res.render("userPref/cage", {
+            article: article,
+            userPref: {
+              fighter: fighter,
               user: user
-            }
-          })
+            },
+            scripts: ["cage.js"]
+          });
         })
 
-        .catch(dbErr =>{
-          console.log('there is an error',dbErr);
-        })
-      })
-    })
-})
+        .catch(dbErr => {
+          console.log("there is an error", dbErr);
+        });
+    });
+  });
+});
 
-router.get("/userInfo", (req,res,next)=>{
+router.get("/userInfo", (req, res, next) => {
   const userId = req.session.currentUser._id;
   Users.findById(userId)
-    .then(user=> {
-      console.log("user found", user)
+    .then(user => {
+      console.log("user found", user);
       res.send(user);
     })
-    .catch(userErr=> console.log("user fetch error", userErr))
-})
+    .catch(userErr => console.log("user fetch error", userErr));
+});
 
-router.post("/newFighter",(req,res,next)=>{
-  let fighterId= req.body.fighter_id;
-  Users.findOneAndUpdate({_id: req.session.currentUser._id}, {fighter: fighterId}, {new:true})
-  .then(dbRes => console.log("new fighter updated", dbRes))
-  .catch(dbErr => console.log("Error in fighter update", dbErr))
-})
+router.post("/newFighter", (req, res, next) => {
+  let fighterId = req.body.fighter_id;
+  Users.findOneAndUpdate(
+    { _id: req.session.currentUser._id },
+    { fighter: fighterId },
+    { new: true }
+  )
+    .then(dbRes => console.log("new fighter updated", dbRes))
+    .catch(dbErr => console.log("Error in fighter update", dbErr));
+});
 
-router.post("/newSports",(req,res,next)=>{
-  let sportsArray= req.body.selectedSports;
-  Users.findOneAndUpdate({_id: req.session.currentUser._id}, {leagues: sportsArray}, {new:true})
-  .then(dbRes => console.log("sports list updated", dbRes))
-  .catch(dbErr => console.log("Error in sports list update", dbErr))
-})
-
+router.post("/newSports", (req, res, next) => {
+  let sportsArray = req.body.selectedSports;
+  Users.findOneAndUpdate(
+    { _id: req.session.currentUser._id },
+    { leagues: sportsArray },
+    { new: true }
+  )
+    .then(dbRes => console.log("sports list updated", dbRes))
+    .catch(dbErr => console.log("Error in sports list update", dbErr));
+});
 
 module.exports = router;
